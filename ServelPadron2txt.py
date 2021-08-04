306750ae4027cfdb771a7f55eb5a285640662a0a
alvaroj-uv <alvaro.jeria@uv.cl>
2021-08-04 12:50:17 -0400

    Separando codigo

--- ServelPadron2txt.py
+++ ServelPadron2txt.py
@@ -135,23 +135,4 @@ class ImageHandler:
         return self.treated_image
 
 
-class PdfToFile:
-    def __init__(self, inputpdffile, hojas):
-        self.pdffile = inputpdffile
-        self.numhojas = hojas
-        self.imagehandler = ImageHandler()
-        self.OCR = EasyOCR()
-        self.PDF = PdfHandler(self.pdffile)
-        self.File = Outputhandler()
 
-    def process(self):
-        for hoja in range(0, self.numhojas):
-            imagen_hoja = self.PDF.get_image_from_page(hoja)
-            imagen_hoja_tratada = self.imagehandler.treat_image(imagen_hoja)
-            self.OCR.get_text_from_image(imagen_hoja_tratada)
-            self.File.write(self.OCR)
-            print('Hoja' + str(hoja))
-
-
-prueba = PdfToFile('A02103.pdf',2)
-prueba.process()
---
+++ main.py
@@ -0,0 +1,22 @@
+from ServelPadron2txt import ImageHandler,OCRHandler,EasyOCR,PdfHandler,Outputhandler
+
+class PdfToFile:
+    def __init__(self, inputpdffile, hojas):
+        self.pdffile = inputpdffile
+        self.numhojas = hojas
+        self.imagehandler = ImageHandler()
+        self.OCR = EasyOCR()
+        self.PDF = PdfHandler(self.pdffile)
+        self.File = Outputhandler()
+
+    def process(self):
+        for hoja in range(0, self.numhojas):
+            imagen_hoja = self.PDF.get_image_from_page(hoja)
+            imagen_hoja_tratada = self.imagehandler.treat_image(imagen_hoja)
+            self.OCR.get_text_from_image(imagen_hoja_tratada)
+            self.File.write(self.OCR)
+            print('Hoja' + str(hoja))
+
+
+prueba = PdfToFile('A02103.pdf',2)
+prueba.process()
